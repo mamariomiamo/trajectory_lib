5b7aa0ce536864fa4175e3a3085fab7db44063e8
zhengtian <mazt1025@hotmail.com>
2022-11-29 16:40:15 +0800

    Fix distance calculation

--- matlab/close_form/close_form_segment_duration/calDistance.m
+++ matlab/close_form/close_form_segment_duration/calDistance.m
@@ -1,5 +1,5 @@
 function total_dist = calDistance(waypts, k_segment)
 total_dist = 0;
-for i=2:k_segment
-    total_dist = total_dist + norm(waypts(i,:) - waypts(i-1,:));
+for i=1:k_segment
+    total_dist = total_dist + norm(waypts(:,i+1) - waypts(:,i));
 end
